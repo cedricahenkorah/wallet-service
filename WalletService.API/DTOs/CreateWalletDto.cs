b8e7ff787972943cd4babb9e9dd49350facfdf1a
cedricahenkorah <cedricahenkorah@gmail.com>
2025-01-27 00:13:18 +0000

    feat: add validation attributes to CreateWalletDto and UserDto properties

--- WalletService.API/DTOs/CreateWalletDto.cs
+++ WalletService.API/DTOs/CreateWalletDto.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using WalletService.API.Enums;
 
@@ -5,16 +6,23 @@ namespace WalletService.API.DTOs
 {
     public class CreateWalletDto
     {
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Type is required")]
+        [EnumDataType(typeof(WalletType), ErrorMessage = "Type must be either 'Momo' or 'Card'")]
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public WalletType Type { get; set; }
 
+        [Required(ErrorMessage = "Account Number is required")]
         public string AccountNumber { get; set; }
 
+        [Required(ErrorMessage = "Account Scheme is required")]
+        [EnumDataType(typeof(AccountScheme), ErrorMessage = "Account Scheme is an invalid value.")]
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public AccountScheme AccountScheme { get; set; }
 
+        [Required(ErrorMessage = "Owner is required")]
         public string Owner { get; set; }
     }
 }
--- WalletService.API/DTOs/UserDto.cs
+++ WalletService.API/DTOs/UserDto.cs
@@ -4,8 +4,10 @@ namespace WalletService.API.DTOs
 {
     public class UserDto
     {
+        [Required(ErrorMessage = "Phone number is required")]
         public required string PhoneNumber { get; set; }
 
+        [Required(ErrorMessage = "Password is required")]
         [MinLength(6)]
         public required string Password { get; set; }
     }
